d9929fa53dc2f6d14c94a247254531f784ef5c82
tejasprojects2026 <tejasprojects2026@users.noreply.github.com>
2026-05-27 14:44:05 +0530

    Updated website changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import "./lib/i18n";
+import i18n from "./lib/i18n";
 import App from "./App";
+
+void i18n;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
